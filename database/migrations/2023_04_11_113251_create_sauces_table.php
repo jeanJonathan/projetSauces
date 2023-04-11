505591448bf9640c7dba869dc31bda75824c582a
Jean Jonathan <jeanjonathank2@gmail.com>
2023-04-11 20:33:41 +0200

    Initial commit

---
+++ database/migrations/2023_04_11_113251_create_sauces_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sauces', function (Blueprint $table) {
+            $table->id();
+            $table->string('userId');
+            $table->string('name');
+            $table->string('manufacturer');
+            $table->text('description');
+            $table->string('mainPepper');
+            $table->string('imageUrl');
+            $table->integer('heat');
+            $table->integer('likes')->default(0);
+            $table->integer('dislikes')->default(0);
+            $table->json('usersLiked')->nullable();
+            $table->json('usersDisliked')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sauces');
+    }
+};
